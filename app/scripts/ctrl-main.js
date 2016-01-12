b345c0b0f40f62ef0440676a701dfc90656018b8
Dave Riedstra <dave.riedstra@gmail.com>
2016-01-12 10:51:47 -0800

    remove commented original addMember reference code

--- app/scripts/ctrl-main.js
+++ app/scripts/ctrl-main.js
@@ -185,76 +185,9 @@ app.controller('MainInteractionController',function($scope,FURL,Auth,Phased,$htt
   };
 
   // adds a new member to the current team
-  $scope.addMember = function(names) {
+  $scope.addMember = function(newUser) {
     _gaq.push(['_trackEvent', 'Team', 'Add member']);
-    Phased.addMember(names);
-  	/*var ref = new Firebase(FURL);
-    // grab all users and see if they match an email in the system
-    ref.child('profile').once('value', function(data){
-      data = data.val();
-
-      var selectedUID = Object.keys(data);
-      var isSet = false;
-
-      // if this email matches the one from the profile page assign this team to their account
-      for(var y = 0; y < selectedUID.length; y++){
-        console.log('test3');
-        if(names.email == data[selectedUID[y]].email){
-          isSet = true;
-          //get the key of the uid
-
-          //save to new node so that zapier can email.
-          ref.child('team-invite-existing-member').push({teams : { 0 : Auth.team},email : names.email, inviteEmail: $scope.currentUser.email, inviteName: $scope.currentUser.name });
-
-          //push new team to member
-          ref.child('profile').child(selectedUID[y]).child('teams').push(Auth.team);
-          break;
-        }
-      }
-      // if no matches are found create a profile-in-waiting with this team assigned.
-      if(!isSet){
-        console.log(names.email, $scope.currentUser);
-
-        // loop profile-in-waiting to find a match
-        ref.child('profile-in-waiting').once('value', function(data){
-          data = data.val();
-          var selectedUID = Object.keys(data);
-          var thisSet = false;
-          for(var y = 0; y < selectedUID.length; y++){
-            console.log(data[selectedUID[y]].email);
-            if(names.email == data[selectedUID[y]].email){
-              thisSet = true;
-              //check if email already has team attached
-              var userTeams = Object.keys(data[selectedUID[y]].teams);
-              var profileOfUser = data[selectedUID[y]];
-              var change = false;
-
-              for(var u = 0; u < userTeams.length; u++){
-                if(profileOfUser.teams[userTeams[u]] == Auth.team){
-                  break;
-                }else{
-                  change = true;
-                  break;
-                }
-              }
-              if(change){
-                //push new team to member
-                ref.child('profile-in-waiting').child(selectedUID[y]).child('teams').push(Auth.team);
-                //sendTheMail(msg);
-                break;
-              }
-            }
-          }
-          if(!thisSet){
-            ref.child('profile-in-waiting').push({teams : { 0 : Auth.team},email : names.email, inviteEmail: $scope.currentUser.email, inviteName: $scope.currentUser.name });
-            ref.child('profile-in-waiting2').push({teams : { 0 : Auth.team},email : names.email, inviteEmail: $scope.currentUser.email, inviteName: $scope.currentUser.name });
-
-
-            //sendTheMail(msg);
-          }
-        });
-      }
-    });*/
+    Phased.addMember(newUser);
     $('#myModal').modal('toggle');
   };
 
