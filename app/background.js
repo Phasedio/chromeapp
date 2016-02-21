c42763a79e371d6d7cf1ac1d3e0bb8eccc0896a6
Brian Best <brian.brianb64@gmail.com>
2016-02-21 12:14:06 -0400

    Notification issue is now fixed

--- app/background.js
+++ app/background.js
@@ -18,66 +18,51 @@ var FURL = 'https://phaseddev.firebaseio.com/';
 var ref = new Firebase(FURL);
 console.log("this is working in the background!");
 
-
-// var authClient = new FirebaseSimpleLogin(myRef, function(error, user) {
-//   if (error) {
-//     // an error occurred while attempting login
-//     console.log(error);
-//     alert(error);
-//   } else if (user) {
-//     // user authenticated with Firebase
-//     console.log("User ID: " + user.uid + ", Provider: " + user.provider);
-//     currentUser = user;
-//     alert(currentUser);
-//   } else {
-//   	alert('Logged out');
-//     // user is logged out
-//   }
-// });
 ref.onAuth(function(authData) {
 	if (authData) {
-    console.log(authData.uid);
+    //console.log(authData.uid);
     ref.child('profile').child(authData.uid).once('value', function(user) {
       user = user.val();
       currentUser = user;
-      console.log(user);
-      console.log(user.curTeam);
+      //console.log(user);
+      //console.log(user.curTeam);
 
-	    //  ref.child('team').child(user.curTeam).child('members').on('child_changed', function(childSnapshot) {
-	    //    var newUpdate = childSnapshot.val();
-	    //    //console.log(newUpdate.name);
-	    //    //console.log(newUpdate);
-      //    //console.log(newUpdate.user, authData.uid);
-       //
-      //    ref.child('profile').child(newUpdate.currentStatus.user).once('value', function(notifier) {
-      //      notifier = notifier.val();
-      //      console.log(notifier.gravatar);
-      //      var now = new Date().getTime;
-      //      var r = now - newUpdate.currentStatus.time;
-      //      if (notifier.email == user.email) {
-      //        console.log('wont show anything');
-      //      } else if(r > 50 ){
-      //        console.log('likely not an update');
-      //      }else {
-       //
-      //        spawnNotification(newUpdate.currentStatus.name + " - Phased.io", notifier.gravatar, notifier.name);
-       //
-      //        function spawnNotification(theBody, theIcon, theTitle) {
-       //
-      //          var options = {
-      //            body: theBody,
-      //            icon: theIcon
-      //          };
-       //
-      //          var n = new Notification(theTitle, options);
-      //          setTimeout(n.close.bind(n), 5000);
-      //        }
-      //      }
-      //    });
-       //
-       //
-       //
-	    //    });
+	     ref.child('team').child(user.curTeam).child('members').on('child_changed', function(childSnapshot) {
+	       var newUpdate = childSnapshot.val();
+	       //console.log(newUpdate.name);
+	       //console.log(newUpdate);
+         //console.log(newUpdate.user, authData.uid);
+
+         ref.child('profile').child(newUpdate.currentStatus.user).once('value', function(notifier) {
+           notifier = notifier.val();
+           //console.log(newUpdate);
+           var now = new Date().getTime();
+           var r = now - newUpdate.currentStatus.time;
+           console.log(now +" - "+ newUpdate.currentStatus.time + " = "+ r);
+           if (notifier.email == user.email) {
+             console.log('wont show anything');
+           } else if(r > 1000 ){
+             console.log('likely not an update');
+           }else {
+
+             spawnNotification(newUpdate.currentStatus.name + " - Phased.io", notifier.gravatar, notifier.name);
+
+             function spawnNotification(theBody, theIcon, theTitle) {
+
+               var options = {
+                 body: theBody,
+                 icon: theIcon
+               };
+
+               var n = new Notification(theTitle, options);
+               setTimeout(n.close.bind(n), 5000);
+             }
+           }
+         });
+
+
+
+	       });
 
 	    });
 	}
